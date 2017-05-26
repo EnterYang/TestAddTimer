21dc97f80805d56fd5933ae0f5d9dbabe5e02f42
杨青旭 <yangqingxu90@gmail.com>
2017-05-26 17:02:46 +0800

    add timer

---
+++ Podfile
@@ -0,0 +1,12 @@
+platform :ios, '8.0'
+
+target ’Test’ do
+
+pod 'AFNetworking', '~> 3.0.4'
+pod 'SDWebImage', '~> 3.7.4'
+pod 'Masonry', '~> 0.6.4'
+pod 'MJExtension', '~> 3.0.10'
+pod 'MJRefresh', '~> 3.1.0'
+pod 'SVProgressHUD', '~> 2.0.3'
+
+end
---
+++ Test/ViewController.m
@@ -0,0 +1,97 @@
+//
+//  ViewController.m
+//  test
+//
+//  Created by yangqingxu on 2017/5/26.
+//  Copyright © 2017年 yangqingxu. All rights reserved.
+//
+
+#import "ViewController.h"
+#import <Masonry/Masonry.h>
+static const int pageCount = 3;
+@interface ViewController ()<UIScrollViewDelegate>
+@property (weak, nonatomic) IBOutlet UIScrollView *scrollView;
+@property (weak, nonatomic) IBOutlet UIPageControl *pageControl;
+@property (strong, nonatomic) NSTimer *timer;
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    self.pageControl.numberOfPages = pageCount;
+    self.pageControl.userInteractionEnabled = NO;
+}
+- (void)viewDidAppear:(BOOL)animated{
+    [super viewDidAppear:YES];
+    
+    CGFloat W = self.scrollView.bounds.size.width;
+    for (int i = 1; i<4; i++) {
+        UIImageView *imageView = [[UIImageView alloc]initWithImage:[UIImage imageNamed:[NSString stringWithFormat:@"new_feature_%zd",i]]];
+        imageView.contentMode = UIViewContentModeScaleToFill;
+        [self.scrollView addSubview:imageView];
+        
+        [imageView mas_makeConstraints:^(MASConstraintMaker *make) {
+            make.top.mas_equalTo(self.scrollView.mas_top);
+            make.left.mas_equalTo(self.scrollView.mas_left).offset((i-1)*W);
+            make.size.mas_equalTo(self.scrollView.bounds.size);
+        }];
+    }
+    self.scrollView.contentSize = CGSizeMake(W*pageCount, 0);
+    self.scrollView.pagingEnabled = YES;
+    self.scrollView.delegate = self;
+    [self addTimer];
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+- (void)scrollViewDidScroll:(UIScrollView *)scrollView{
+    
+    CGFloat offsetX = scrollView.contentOffset.x;
+    
+    float pageFloat = offsetX / scrollView.frame.size.width;
+    
+    self.pageControl.currentPage = (int)(pageFloat+0.5);
+}
+
+- (void)addTimer
+{
+    self.timer = [NSTimer scheduledTimerWithTimeInterval:3.0 target:self selector:@selector(nextImage) userInfo:nil repeats:YES];
+    [[NSRunLoop currentRunLoop] addTimer:self.timer forMode:NSRunLoopCommonModes];
+}
+
+- (void)removeTimer
+{
+    [self.timer invalidate];
+    self.timer = nil;
+}
+
+- (void)nextImage
+{
+    NSInteger page = 0;
+    if (self.pageControl.currentPage == pageCount - 1) {
+        page = 0;
+    } else {
+        page = self.pageControl.currentPage + 1;
+    }
+    
+    CGFloat offsetX = page * self.scrollView.frame.size.width;
+    CGPoint offset = CGPointMake(offsetX, 0);
+    [self.scrollView setContentOffset:offset animated:YES];
+}
+
+
+- (void)scrollViewWillBeginDragging:(UIScrollView *)scrollView
+{
+    [self removeTimer];
+}
+
+- (void)scrollViewDidEndDragging:(UIScrollView *)scrollView willDecelerate:(BOOL)decelerate
+{
+    [self addTimer];
+}
+
+@end
